82df4fdcfa579862116bdf7e4b36698595024fa4
Vedant Sonawane <98942578+Vedant2s@users.noreply.github.com>
2023-05-10 15:56:10 +0530

    Update amazonQuestion.c

--- amazonQuestion.c
+++ amazonQuestion.c
@@ -14,35 +14,38 @@ int main()
     // }
     // printf("%d",max);
     int ar[10]={-7,-1,-6,3,7,1,5,-1,-10,9};
-    int visited[10]={},u=0,count=0;
+    int visited[10][3]={},u=0,count=0;
     for(int i=0;i<10;i++){
         for(int j=i+1;j<10;j++){
             for(int k=j+1;k<10;k++){
                 if(ar[i]+ar[j]+ar[k]==0)
                 {
-                    if(u==0){visited[u]=ar[i];
-                    u++;
-                    visited[u]=ar[j];
-                    u++;
-                    visited[u]=ar[k];
-                    u++;}
-                    if(u!=0){
+                    if(u!=0){for(int y=0;y<u;y++){
                         for(int p=0;p<u;p++)
-                        if(ar[i]==visited[p])
+                        if(ar[i]==visited[y][p])
                         count++;
                         for(int p=0;p<u;p++)
-                        if(ar[j]==visited[p])
+                        if(ar[j]==visited[y][p])
                         count++;
                         for(int p=0;p<u;p++)
-                        if(ar[k]==visited[p])
+                        if(ar[k]==visited[y][p])
                         count++;
+                        if(count<3)
+                        count=0;
                     }
+                    }
+                    if(1){visited[u][0]=ar[i];
+                    visited[u][1]=ar[j];
+                    visited[u][2]=ar[k];
+                    u++;}
                     if(count!=3)
                     printf("%d,%d,%d\n",ar[i],ar[j],ar[k]);
+                    printf("count %d\n",count);
                     count=0;
                 }
             }
         }
     }
+    printf("%d",count);
     return 0;
 }
